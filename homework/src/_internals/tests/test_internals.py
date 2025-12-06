810929953d6a46f97f1fc56de072fbff7e88e18d
Sebaxwz <jperezcor@unal.edu.co>
2025-12-06 07:54:53 -0500

    ready

--- homework/src/_internals/tests/test_internals.py
+++ homework/src/_internals/tests/test_internals.py
@@ -2,12 +2,16 @@ import os
 import shutil
 import sys
 
+from ..write_word_counts import write_word_counts
+
+from ..count_words import count_words
+
+from ..split_into_words import split_into_words
+
+from ..preprocess_lines import preprocess_lines
+
 from ...wordcount import (
-    count_words,
     parse_args,
-    preprocess_lines,
-    split_into_words,
-    write_word_counts,
 )
 from ..read_all_lines import read_all_lines
 
